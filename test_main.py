80400dfa664a34587e511f5c07e6f7796bb5d51c
zrow <mrzrow@yandex.ru>
2024-09-26 15:38:30 +0300

    updated tests

--- test_main.py
+++ test_main.py
@@ -1,48 +1,22 @@
-# from fastapi.testclient import TestClient
-# from main import app
-#
-# client = TestClient(app)
-#
-#
-# def test_get_product_by_nonexistent_id():
-#     response = client.get('/products/-1/')
-#     assert response.status_code == 404
-#     assert response.json() == {'detail': 'Product not found'}
-#
-#
-# def test_get_product_by_id():
-#     response = client.get('/products/1/')
-#     assert response.status_code == 200
-#     assert response.json() == {
-#         "name": "Молоко",
-#         "description": "Дает корова",
-#         "price": 120,
-#         "amount": 8,
-#         "id": 1
-#     }
-
-import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
 from main import app
 
-
-@pytest.mark.asyncio
-async def test_get_product_by_nonexistent_id():
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.get('/products/-1/')
-        assert response.status_code == 404
-        assert response.json() == {'detail': 'Product not found'}
+client = TestClient(app)
 
 
-@pytest.mark.asyncio
-async def test_get_product_by_id():
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        response = await client.get('/products/1/')
-        assert response.status_code == 200
-        assert response.json() == {
-            "name": "Молоко",
-            "description": "Дает корова",
-            "price": 120,
-            "amount": 8,
-            "id": 1
-        }
+def test_get_product_by_nonexistent_id():
+    response = client.get('/products/-1/')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Product not found'}
+
+
+def test_get_product_by_id():
+    response = client.get('/products/1/')
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "Молоко",
+        "description": "Дает корова",
+        "price": 120,
+        "amount": 8,
+        "id": 1
+    }
